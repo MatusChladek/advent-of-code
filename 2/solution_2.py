90c59049d240b80b866b30fca585077b72344649
Matus Chladek <mchladek@zetaglobal.com>
2020-12-08 21:36:34 +0100

    2

--- 2/solution_2.py
+++ 2/solution_2.py
@@ -18,8 +18,116 @@ Each line gives the password policy and then the password. The password policy i
 In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
 
 How many passwords are valid according to their policies?
+
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
 """
+from typing import List, Optional, Match, AnyStr, Iterator
+
+from dataclasses import dataclass
+
+import re
+
+from abc import ABCMeta, abstractmethod
+
+file_relative_path = "data_2.txt"
+
+
+class Password(metaclass=ABCMeta):
+    char: str
+    password: str
+
+    @abstractmethod
+    def is_valid(self) -> bool:
+        raise NotImplementedError
+
+
+@dataclass
+class PasswordPolicy1(Password):
+    char: str
+    min_occurence: int
+    max_occurence: int
+    password: str
+
+    def is_valid(self) -> bool:
+        return (
+            self.min_occurence <= self.password.count(self.char) <= self.max_occurence
+        )
+
+
+@dataclass
+class PasswordPolicy2(Password):
+    char: str
+    positive: int
+    negative: int
+    password: str
+
+    def is_valid(self) -> bool:
+        sum_of_true = (self.password[self.positive - 1] == self.char) + (
+            self.password[self.negative - 1] == self.char
+        )
+        return sum_of_true == 1
+
+
+def get_data() -> List[str]:
+    with open(file_relative_path) as f:
+        data = [line.rstrip() for line in f]
+        return data
+
+
+def is_valid_password_1(line: str) -> bool:
+    p = re.compile("^([0-9]+)-([0-9]+) ([a-zA-Z]): ([^ ]+)$")
+    groups = p.match(line)
+
+    if groups is not None:
+        c = PasswordPolicy1(
+            char=groups.group(3),
+            min_occurence=int(groups.group(1)),
+            max_occurence=int(groups.group(2)),
+            password=groups.group(4),
+        )
+
+        return c.is_valid()
+    else:
+        raise BaseException("Pattern not found.")
+
+
+def is_valid_password_2(line: str) -> bool:
+    p = re.compile("^([0-9]+)-([0-9]+) ([a-zA-Z]): ([^ ]+)$")
+    groups = p.match(line)
+
+    if groups is not None:
+        c = PasswordPolicy2(
+            char=groups.group(3),
+            positive=int(groups.group(1)),
+            negative=int(groups.group(2)),
+            password=groups.group(4),
+        )
+        return c.is_valid()
+    else:
+        raise BaseException("Pattern not found.")
 
 
 if __name__ == "__main__":
-    exit()
+    data = get_data()
+
+    results_policy_1: Iterator[bool] = map(is_valid_password_1, data)
+    print(
+        f"There is <{sum(results_policy_1)}> out of <{len(data)}> total passwords according to policy 1."
+    )
+
+    results_policy_2: Iterator[bool] = map(is_valid_password_2, data)
+    print(
+        f"There is <{sum(results_policy_2)}> out of <{len(data)}> total passwords according to policy 2."
+    )
